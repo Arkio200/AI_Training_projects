ea775ae2979e65594d2aec5cd4b5bf26b8e9eb75
Laurentiu Gadirca <gadircalaurentiu200@gmail.com>
2025-10-24 23:28:03 +0200

    Project_8

--- Grade_Analyzer.py
+++ Grade_Analyzer.py
@@ -1,32 +1,19 @@
 def main():
-    dictionaries = []
-    while True:
-        user_input = input("Student and score: ")
-        if len(user_input.split()) == 2 and user_input.lower != "stop":
-            name, score = user_input.split()
-            dictionaries.append({"name": name, "score": score})
-            continue
-        elif user_input.lower == "stop":
-            break
-        break
-    scores = []
-    for dictionary in dictionaries:
-        scores.append(int(dictionary.get("score")))
-    max_score = max(scores)
-    min_score = min(scores)
+    user_input = input("Scores: ")
+    scores = [int(x) for x in user_input.split()]
+    
+    if not all(0 <= n <= 100 for n in scores):
+        print("Please enter valid scores between 0 and 100.")
+        return
+    
+    passing = sum(1 for n in scores if n >= 60)
+    failing = len(scores) - passing
+    
     print(f"Average: {sum(scores) / len(scores):.1f}")
-    print(f"Highest: {max(dictionaries).get("name")} ({max_score})")
-    print(f"Lowest: {min(dictionaries).get("name")} ({min_score})")
-
-
-
-#    values = []
-#    print(f"Average: {sum(values) / len(values):.1f}")
-#    print(f"Highest: {dictionary[name]} ({max(values)})")
-#    print(f"Lowest: {dictionary[name]} ({min(values)})")
+    print(f"Highest: {max(scores)}")
+    print(f"Lowest: {min(scores)}")
+    print(f"Passing count: {passing}")
+    print(f"Failing count: {failing}")
 
 if __name__ == "__main__":
     main()
-    
-
-        
